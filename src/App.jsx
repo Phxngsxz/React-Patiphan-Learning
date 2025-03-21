44266c1ece22b1de8b0d88b6ba96ee4f73083724
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-21 16:25:21 +0700

    react fragment staging ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,26 @@ import MyButton from './components/MyButton'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("Phongsaphon somsin")
+  const [ifTrue, setIfTrue] = useState(true)
+
+  const user = {
+    firstName: "Mai",
+    lastName: "Kagenou",
+    imgPic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThvLHSQNGAB1RJVNtDavx0XhhXMKA-rTnPUw&s"
+  }
 
   return (
-    <div>
+    // React Fragment
+    <>
       <h1>Welcome to my app</h1>
+      <p>My name is {user.firstName} {user.lastName}</p>
+      <img src={user.imgPic} width="150" height="150" alt={user.firstName}/>
+      <p>{count}</p>
       <MyButton />
-    </div>
+      {/* Ternary Operator */}
+      {ifTrue ? "Yes, it's true" : "No, it's false"}
+    </>
   )
 }
 
